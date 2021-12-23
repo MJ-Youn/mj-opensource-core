70a098717990288ed4e98067fd0c58bb0c355dc2
MJ Youn <yun0244@naver.com>
2021-12-23 15:11:14 +0900

    init

---
+++ src/test/java/io/github/mjyoun/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mjyoun.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
